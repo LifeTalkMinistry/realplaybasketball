aee311e1f7d17bccce798725e1e475dba9d27a8c
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-03 14:12:09 +0800

    Refresh assets for Settings carousel

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260903-3v3-team-carousel-v1';
+  const version = '20260903-settings-carousel-v2';
   const html = document.documentElement;
   html.classList.add('js');
 
