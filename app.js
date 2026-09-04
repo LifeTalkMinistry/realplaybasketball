7f86d5d9ed867d67e7dd5891433965ba3ecf943c
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-04 17:49:15 +0800

    Bust app cache for manual season start flow

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260904-admin-stable-v1';
+  const version = '20260904-season-manual-start-v1';
   const html = document.documentElement;
   html.classList.add('js');
 
