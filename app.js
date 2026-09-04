465d5f891ce9070009a14eee6606db7601a0e03c
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-04 16:35:42 +0800

    Bust cache for 3V3 release verification fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260904-3v3-league-reservation-v2';
+  const version = '20260904-3v3-league-reservation-v3';
   const html = document.documentElement;
   html.classList.add('js');
 
