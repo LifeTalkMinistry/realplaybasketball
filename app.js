5b4bb62577ef750955d891ee372365c335c831c2
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-05 22:56:09 +0800

    Bust cache for dedicated career metric pages

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260905-career-metrics-carousel-v1';
+  const version = '20260905-career-metric-pages-v2';
   const html = document.documentElement;
   html.classList.add('js');
 
