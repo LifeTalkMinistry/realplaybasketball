f9a21bb363aa3b96daa414f14ea6beba6d48707d
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-06 05:53:19 +0800

    Bump main menu loader cache to v6

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260906-main-menu-physics-v5';
+  const version = '20260906-main-menu-physics-v6';
   const html = document.documentElement;
   html.classList.add('js');
 
