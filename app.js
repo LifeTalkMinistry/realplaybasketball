6be1bf239683192d3896c6b0766c94e53fa3326e
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-07 06:57:23 +0800

    Refresh desktop score desk assets

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260907-youtube-source-v1';
+  const version = '20260907-desktop-video-score-desk-v1';
   const html = document.documentElement;
   html.classList.add('js');
 
