040e6d88d7a28bd3fd832dec3ea183f63350a75f
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-16 08:58:23 +0800

    Bump doctrine brand skin assets

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260915-home-doctrine-hub-v66';
+  const version = '20260915-home-doctrine-brand-v67';
   const html = document.documentElement;
   html.classList.add('js', 'rp-shell-booting');
 
