5a3759053bd5c4f7682f49fcde0c69b15d086603
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-16 08:47:01 +0800

    Bump doctrine hub asset version

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260915-home-why-real-play-v65';
+  const version = '20260915-home-doctrine-hub-v66';
   const html = document.documentElement;
   html.classList.add('js', 'rp-shell-booting');
 
