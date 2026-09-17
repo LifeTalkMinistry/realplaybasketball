d32599053e57c1334199c854b3c80d279c43c0ff
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-18 07:27:24 +0800

    Bump app cache version for profile badges

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260917-player-connections-v81';
+  const version = '20260917-profile-badges-v82';
   const html = document.documentElement;
   html.classList.add('js', 'rp-shell-booting');
 
