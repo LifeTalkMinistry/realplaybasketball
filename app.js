f46f971c87dc6af33e3c9c606fc60a6737f5ccb4
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-18 11:20:12 +0800

    Bump app assets for Home spots-left indicator

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260918-readable-reservation-snapshot-v90';
+  const version = '20260918-home-spots-left-v91';
   const html = document.documentElement;
   html.classList.add('js', 'rp-shell-booting');
 
