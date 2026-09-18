69a83c4132c58dba5947285bdada4a9f0aa2f8dd
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-18 10:34:24 +0800

    Bump app bundle for unclaimed player scheduling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260917-profile-badges-v85';
+  const version = '20260918-unclaimed-admin-schedule-v86';
   const html = document.documentElement;
   html.classList.add('js', 'rp-shell-booting');
 
