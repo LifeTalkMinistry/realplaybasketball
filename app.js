500437626951b4435937aaf7c0e153794d31f9aa
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-26 01:12:06 +0800

    Bust enhancement cache for volunteer header cleanup

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (() => {
-  const version = '20260925-support-role-details-v120';
+  const version = '20260925-support-header-back-v121';
   const html = document.documentElement;
   html.classList.add('js', 'rp-shell-booting');
 
